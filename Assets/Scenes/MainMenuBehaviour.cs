b515c56ba2a694280e9a91dac8bf2d1b726c948f
Patrick Goldinger <patrick.goldinger.02@gmail.com>
2019-10-14 15:22:35 +0200

    Added MainMenu Scene.
    Additional ProjectSettings changes.

---
+++ Assets/Scenes/MainMenuBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Button__LevelTest__OnClick() {
+
+    }
+
+    public void Button__Quit__OnClick() {
+        // Save game data
+
+        // Close application
+        Debug.Log("Quitting WizGame!");
+        Application.Quit();
+    }
+}
